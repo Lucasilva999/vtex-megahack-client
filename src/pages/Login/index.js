8651cc1684003c5cbfbb53314055de3ccaf0af67
Renato <renato@Renatos-iMac.local>
2020-05-02 16:46:25 -0300

    Atualização marketplace

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,54 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
-import './styles.css';
+import "./styles.css";
 
 export default function Login() {
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const history  = useHistory();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  useEffect(() => {
+    const mail = localStorage.getItem("email");
 
-
-    useEffect(() => {
-        
-        const mail = localStorage.getItem('email')
-
-        if (mail) {
-            history.push('/');        
-        }
-
-    }, [])
+    if (mail) {
+      history.push("/");
+    }
+  }, []);
 
   function handleLogin() {
-
-    localStorage.setItem('email', email);
-    history.push('/');
-
+    localStorage.setItem("email", email);
+    history.push("/");
   }
 
   return (
     <div>
-        <section className="form-section">
-            <h1></h1>
+      <section className="form-section">
+        <h1></h1>
 
-            <div className="form-wrapper">
-                <form>
-                <div className="input-block">
-                    <label>Email</label>
-                    <input type="email" id="login-email" value={email} onChange={e => setEmail(e.target.value)} />
-                </div>
-                <div className="input-block">
-                    <label>Senha</label>
-                    <input type="password" id="login-password" value={password} onChange={e => setPassword(e.target.value)} />
-                </div>
-                <button type="submit" onClick={handleLogin} className="btn-login">Login</button>
-                </form>
+        <div className="form-wrapper">
+          <form>
+            <div className="input-block">
+              <label>Email</label>
+              <input
+                type="email"
+                id="login-email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
-            </section>
+            <div className="input-block">
+              <label>Senha</label>
+              <input
+                type="password"
+                id="login-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button type="submit" onClick={handleLogin} className="btn-login">
+              Login
+            </button>
+          </form>
+        </div>
+      </section>
 
-        <ul className="squares"></ul>
-
+      <ul className="squares"></ul>
     </div>
   );
 }

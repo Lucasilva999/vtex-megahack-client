41292d70503c4b676af9e919df291cc4f5d8e20d
Lucasilva88 <Lucasilva999@hotmail.com.br>
2020-05-02 16:12:23 -0300

    Merge

---
+++ src/pages/Home/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import chart from "../../assets/grafico.png";
+
+import Menu from "../shared/menu";
+
+import "./styles.css";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="menu">
+        <Menu />
+      </div>
+
+      <div>
+        <div className="charts">
+          <img src={chart} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}

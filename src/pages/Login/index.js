50568f12e0fb8c28331edbfe85a22158bf21ae43
Brenno Hoffmann <brenno_hoffmann@outlook.com>
2020-05-02 14:26:52 -0300

    adicionando sass no projeto e convertendo css do login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import './styles.css';
+import '../../assets/sass/main.scss';
 
 export default function Login() {
 
